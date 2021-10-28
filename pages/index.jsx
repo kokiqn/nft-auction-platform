4779a6399922b2dbd3c44963f12373e03d8e7e4b
kokiqn <kaloyna.stefanoff@gmail.com>
2021-10-28 18:44:35 +0300

    feat: add Header component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,9 @@
-import Logo from "../src/components/logo/Logo";
+import Header from "../src/components/header/Header"
 
 export default function Index() {
-  return <Logo />;
+  return (
+    <>
+      <Header/>
+    </>
+  )
 }

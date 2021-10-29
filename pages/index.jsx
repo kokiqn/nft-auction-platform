1b8edc7d87a0f581bb498bb32c5dab3989a53ef9
kokiqn <kaloyna.stefanoff@gmail.com>
2021-10-29 11:58:23 +0300

    feat: add Avatar component style: minor change to search input

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../src/components/header/Header"
+import Avatar from "../src/components/avatar/Avatar"
 
 export default function Index() {
   return (
     <>
       <Header/>
+      <Avatar/>
     </>
   )
 }

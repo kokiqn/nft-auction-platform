665463f716b5333419e97a9ca0b9fc2ef5ed3bbb
kokiqn <kaloyna.stefanoff@gmail.com>
2021-11-01 16:42:31 +0200

    fix: props in Card.jsx and millify units style: badge in Avatar.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,22 +2,10 @@ import Header from "../src/components/header/Header"
 import Card from "../src/components/card/Card"
 
 export default function Index() {
-  let defaults = {
-    "name": String,
-    "likes": Number,
-    "mediaUrl": String,
-    "user": {
-      "avatarUrl": String,
-      "verified": Boolean
-    },
-    "price": String,
-    "currency": String
-  }
-  
   return (
     <>
       <Header/>
-      <Card defaults={defaults}/>
+      <Card/>
     </>
   )
 }

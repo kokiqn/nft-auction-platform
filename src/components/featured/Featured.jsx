6ac42363a80a0c430e2b2f83ab3bd1a39afaefdd
kokiqn <kaloyna.stefanoff@gmail.com>
2021-11-05 21:51:36 +0200

    refactor: change urls in items > img property style: smoother animations

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,15 +2,7 @@ import { Container, ImageList, ImageListItem } from '@mui/material'
 import { useRouter } from 'next/router';
 import React from 'react';
 
-import styles from './Features.module.scss'
-
-function srcset(image, size, rows = 1, cols = 1) {
-  return {
-    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${size * rows
-      }&fit=crop&auto=format&dpr=2`,
-  };
-}
+import styles from './Featured.module.scss'
 
 export default function Featured({ items = [] }) {
   const router = useRouter()
@@ -18,18 +10,19 @@ export default function Featured({ items = [] }) {
   return (
     <Container className={styles.container} maxWidth="xl">
       <ImageList
-        sx={{ width: "max", height: 510, padding: 1 }}
+        sx={{ width: "max", height: "max", padding: 1 }}
         variant="quilted"
         cols={6}
         rowHeight={230}
         gap={20}
       >
         {items.map((item) => (
-          <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+          <ImageListItem key={item.image} cols={item.cols || 1} rows={item.rows || 1}>
             <img
               onClick={() => router.push(item.href)}
               className={styles.item}
-              {...srcset(item.img, 121, item.rows, item.cols)}
+              src={item.image}
+              srcSet={item.image}
               alt={item.title}
               loading="lazy"
             />
@@ -42,39 +35,39 @@ export default function Featured({ items = [] }) {
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    image: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500',
     title: 'Breakfast',
     rows: 2,
     cols: 3,
     href: "/about",
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    image: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=500&h=500',
     title: 'Burger',
     href: "/about",
   },
   {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    image: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=500&h=500',
     title: 'Camera',
     href: "/about",
   },
   {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    image: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=500&h=500',
     title: 'Coffee',
     href: "/about",
   },
   {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    image: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=500&h=500',
     title: 'Hats',
     href: "/about",
   },
   {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    image: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=500&h=500',
     title: 'Honey',
     href: "/about",
   },
   {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    image: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=500&h=500',
     title: 'Basketball',
     href: "/about",
   },

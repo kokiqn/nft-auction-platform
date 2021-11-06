f6a0f3b1b017827b993cc4dac05b1391d5bb0c12
kokiqn <kaloyna.stefanoff@gmail.com>
2021-11-06 13:20:15 +0200

    feat: add Collector and CollectorsColumn components style: change to max-width in User

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -6,7 +6,7 @@ import styles from './User.module.scss';
 export default function User({ name = '', info = '', avatar = '', size = 55, verified = false}) {
   return (
     <div className={styles.user} style={{ height: 55 }}>
-      <Avatar url='/images/avatar.png' size={size}/>
+      <Avatar url='/images/avatar.png' size={size} verified/>
       <div>
         <p className={styles.name}>{name}</p>
         <p className={styles.info}>{info}</p>

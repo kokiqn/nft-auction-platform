97062b8aae4b25efba117f1e3d75b22f956b9b0a
kokiqn <kaloyna.stefanoff@gmail.com>
2021-11-08 22:26:44 +0200

    style: fix Live state Card bug feat: add boilerplate for ProductImage feat: add timeLeft prop to dataNfts

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,31 +18,37 @@ export default function Index() {
   const [trendingCards, setTrendingCards] = useState([]);
   const [users, setUsers] = useState([]);
   const [nfts, setNfts] = useState([]);
-    useEffect(() => {
-      dataFeatured[0]["cols"] = 3;
-      dataFeatured[0]["rows"] = 2;
-      dataFeatured.map(obj => obj["href"] = "/about");
+  useEffect(() => {
+    dataFeatured[0]["cols"] = 3;
+    dataFeatured[0]["rows"] = 2;
+    dataFeatured.map(obj => obj["href"] = "/about");
 
-      setFeaturedCards(dataFeatured);
-      setTrendingCards(dataTrending);
+    setFeaturedCards(dataFeatured);
+    setTrendingCards(dataTrending);
 
-      dataUsers.sort(function (a, b) {
-        return b.nfts.length - a.nfts.length;
-      });
+    dataUsers.sort(function (a, b) {
+      return b.nfts.length - a.nfts.length;
+    });
 
-      setUsers(dataUsers.slice(0, 12));
-      setNfts(dataNfts);
-    }, [])
+    setUsers(dataUsers.slice(0, 12));
+
+    dataNfts.map(nft => nft["timeLeft"] = Math.random() * 10000)
+    setNfts(dataNfts);
+  }, [])
 
   return (
     <>
-      <Header/>
-      <Featured items={featuredCards}/>
-      <Trending cards={trendingCards}/>
-      <TopCollectors collectors={users}/>
-      <How/>
-      <Auctions cards={nfts}/>
-      <Footer/>
+      <Header />
+      <Featured items={featuredCards} />
+      <Trending cards={trendingCards} />
+      <TopCollectors collectors={users} />
+      <How
+        title="HOW IT WORKS"
+        description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab dolorum, repellat possimus hic nulla aliquam.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab dolorum, repellat possimus hic nulla aliquam."
+      />
+      <Auctions cards={nfts} />
+      <Footer />
     </>
   )
 }

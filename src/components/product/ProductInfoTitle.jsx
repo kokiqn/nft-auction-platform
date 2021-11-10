b15af269113e7c72d8725a45d785942aa26a6249
kokiqn <kaloyna.stefanoff@gmail.com>
2021-11-10 12:06:27 +0200

    feat: add ProductInfoTitle component style: change to ProductImage img

---
+++ src/components/product/ProductInfoTitle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+import styles from './ProductInfoTitle.module.scss'
+
+export default function ProductInfoTitle({ text }) {
+  return (
+    <div className={styles["product-info-title"]}>
+      <Typography className={styles.title} variant="h1">Title</Typography>
+    </div>
+  )
+}

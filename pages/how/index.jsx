cf38119b6222765005073a04b1cdeb150cb62a07
kokiqn <kaloyna.stefanoff@gmail.com>
2021-11-17 12:29:13 +0200

    style: add .description in module.scss

--- pages/how/index.jsx
+++ pages/how/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Description from '../../src/components/description/Description'
 import Hero from '../../src/components/hero/Hero'
 
 export default function Index() {
   return (
     <div>
       <Hero/>
+      <Description/>
     </div>
   )
 }

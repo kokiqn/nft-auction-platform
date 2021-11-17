e60c3fa476191d53c655e9551e0f30019bcf751b
kokiqn <kaloyna.stefanoff@gmail.com>
2021-11-17 12:23:24 +0200

    feat: add Description component

---
+++ src/components/description/Description.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+
+import styles from './Description.module.scss'
+
+export default function Description({ text, image }) {
+  return (
+    <div className={styles.description}>
+      <Typography className={styles.text} variant="body1">{text}</Typography>
+      <img className={styles.image} src={image}/>
+    </div>
+  )
+}
